12893bc2f50725b85ef182aa9d3857e7f19d66b3
Kevin Bernfeld <kbernfeld@ngpvan.com>
2019-07-15 16:07:01 -0400

    discord bot

---
+++ dias.tracker.discord/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace dias.tracker.discord {
+  public class Program {
+    private static DiscordClient discord;
+    public static async Task Main(string[] args) {
+      discord = new DiscordClient(new DiscordConfiguration {
+        Token = "",
+        TokenType = TokenType.Bot
+      });
+
+      discord.MessageCreated += async e => {
+        Console.WriteLine($"Received message: {e.Message.Content}");
+        if (e.Message.Content.ToLower().StartsWith("!ping")) {
+          await e.Message.RespondAsync("pong!");
+        }
+      };
+
+      await discord.ConnectAsync();
+
+      Console.WriteLine("Connected");
+
+      await Task.Delay(-1);
+    }
+  }
+}
